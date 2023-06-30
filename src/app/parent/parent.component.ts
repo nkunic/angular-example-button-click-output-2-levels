2ef6afce2daea1aec0cc4ab17bbddc183486fcbb
nkunic <45877400+nkunic@users.noreply.github.com>
2023-06-30 14:33:04 +0200

    1 file modified

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -3,6 +3,7 @@ import { Component, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
+  styleUrls: ['./parent.component.scss'],
 })
 export class ParentComponent {
   @Output() clickEvent: EventEmitter<void> = new EventEmitter<void>();
